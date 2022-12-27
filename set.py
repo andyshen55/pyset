32222b982e45c7ab645c99261fa8c4404109ac39
Siddarth Chalasani <darthsid2000@ucla.edu>
2022-12-27 21:42:50 +0530

    added image assets and coded trial board display

--- set.py
+++ set.py
@@ -1,16 +1,32 @@
 import pygame
 from pygame import display
+import os
+from card import Card
+from game import Game
 
 
 # Sets display window size to full screen
+def card_to_img(card):
+    fill_dict = {'filled':'filled', 'lined':'shaded', 'blank':'empty'}
+    return ''.join((card.color, card.shape, fill_dict[card.fill], str(card.number), '.png'))
+
 def set_game_window():
     display.init()
     infoObject = display.Info()
-    display.set_mode((infoObject.current_w, infoObject.current_h))
+    win = display.set_mode((infoObject.current_w, infoObject.current_h))
+    display.set_caption("Set")
+    return win
+
+def draw_window(win, game):
+    win.fill((255, 255, 255))
+    for index, card in enumerate(game.deck):
+        win.blit(pygame.image.load(os.path.join('assets', card_to_img(card))), ((index%7)*300, (index//7)*100))
+    display.update()
 
 
 def game_loop():
-    set_game_window()
+    win = set_game_window()
+    game = Game()
     running = True
 
     while running:
@@ -19,6 +35,10 @@ def game_loop():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            
+        draw_window(win, game)
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
