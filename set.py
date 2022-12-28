452d89111ebc6e2ba25e3e66dfc5cb5814e5005a
Siddarth Chalasani <darthsid2000@ucla.edu>
2022-12-28 17:38:31 +0530

    implemented select card funcitonality

--- set.py
+++ set.py
@@ -20,16 +20,32 @@ def draw_window(win, game):
     win.fill((255, 255, 255))
     for index, card in enumerate(game.cards):
         win.blit(pygame.image.load(os.path.join('assets', card_to_img(card))), ((index%7)*200, display.Info().current_h//3 + (index//7)*100))
+    for card in game.selected:
+        pygame.draw.rect(win, (255, 0, 0), ((card%7)*200, display.Info().current_h//3 + (card//7) * 100, 200, 100), 4)
     display.update()
 
 
 def game_loop():
     win = set_game_window()
     game = Game()
+    board = []
+    for i in range(3):
+        row = []
+        for j in range(7):
+            row.append(pygame.Rect(j*200, display.Info().current_h//3 + i*100, 200, 100))
+        board.append(row)
+
     running = True
 
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                for i in range(3):
+                    for j in range(7):
+                        if board[i][j].collidepoint(x, y):
+                            game.select_card(7*i + j)
+
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
